e87b0f3cbb5fc1c785831b448e5ab3b13275a305
felipevargas-bz <felipe15angel@gmail.com>
2021-07-20 13:09:21 -0500

    TASK0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,8 +8,8 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL;
-	node = malloc(sizeof(binary_tree_t));
 
+	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
 
