33d9a014bed4a00f30c96ee2ab6eba30cb98659c
felipevargas-bz <felipe15angel@gmail.com>
2021-07-20 16:33:46 -0500

    update task1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,11 +27,51 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	else
 	{
-		node->left = parent->left;
-		parent->left->parent = node;
-		node->parent = parent;
-		parent->left = node;
-		node->n = value;
+		if (value > parent->left->n)
+		{
+			node->n = value;
+			node->right = NULL;
+			node->left = parent->left;
+			parent->left->parent = node;
+			node->parent = parent;
+			parent->left = node;
+		}
+		else
+		{
+			node->n = value;
+			node->right = NULL;
+
+			node = insert_node(node, parent->left);
+		}
 	}
+	return (node);
+}
+/**
+ * insert_node - insert a node if the aux is greter than node
+ * @node: node to insert.
+ * @parent: parent of node.
+ * Return: node.
+ */
+binary_tree_t *insert_node(binary_tree_t *node, binary_tree_t *parent)
+{
+	binary_tree_t *aux = parent;
+
+	while (aux->left)
+	{
+		if (aux->n > node->n)
+		{
+			node->parent = aux->parent;
+			aux->parent = node;
+
+			node->parent->left = node;
+			node->left = aux;
+			return (node);
+		}
+		aux = aux->left;
+	}
+	node->parent = aux;
+	aux->left = node;
+	node->left = NULL;
+
 	return (node);
 }
