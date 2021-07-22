73f4c79ed98f50591aacb55961f2b7252748089d
felipevargas-bz <felipe15angel@gmail.com>
2021-07-21 22:03:33 -0500

    TASK 12 WITH RECURSION IN A RETURN

--- binary_trees.h
+++ binary_trees.h
@@ -41,4 +41,5 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 
+
 #endif /*BINARY_TREE_H*/
