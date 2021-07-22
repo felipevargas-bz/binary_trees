8668addb109385474c3c053665b627fec078fa87
felipevargas-bz <felipe15angel@gmail.com>
2021-07-21 23:39:44 -0500

    CODE WITH DESCRIPTION IN OTHER FOMR

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,3 +11,34 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	 tree->left && !tree->right ? 1 : tree->right && !tree->left ? 1 :
 	 binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
+/**
+ *	code in other form - the code in the line 9 is with ternar operator.
+ *
+ *	if (!tree)
+ *		return (0);
+ *	else
+ *	{
+ *		if (tree->right && tree->left)
+ *		{
+ *			return (binary_tree_nodes(tree->left) + 1
+ *				+ binary_tree_nodes(tree->right));
+ *		}
+ *		else
+ *		{
+ *			if (tree->left && !tree->right)
+ *				return (1);
+ *			else
+ *			{
+ *				if (tree->right && !tree->left)
+ *					return (1);
+ *				else
+ *				{
+ *					return (binary_tree_nodes(tree->left) +
+ *						binary_tree_nodes(tree->right));
+ *				}
+ *			}
+ *		}
+ *	}
+ *
+ *
+**/
