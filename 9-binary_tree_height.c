76c5b85cb60c9bcbe37b97be1feea67652ffd85d
vasketz <anfevach@gmail.com>
2021-07-22 01:39:27 +0000

    Update task 9 and header

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,7 +16,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->left)
 		left = binary_tree_height(tree->left) + 1;
 	if (tree->right)
-		right = binary_tree_height(tree->left) + 1;
+		right = binary_tree_height(tree->right) + 1;
 	if (left >= right)
 		return (left);
 	else
